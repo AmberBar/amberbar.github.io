7df12f84fcaea66a585f0e68e2f1dd4308fae4f6
amber.lei <949144210@qq.com>
2022-10-13 23:52:04 +0800

    init

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,58 @@
+import { getDirname, path } from "@vuepress/utils";
+import { config } from "docs-shared";
+import theme from "./theme.js";
+
+const __dirname = getDirname(
+    import.meta.url);
+
+export default config({ indexName: "vuepress-theme-hope-theme-v2" }, {
+    head: [
+        [
+            "meta",
+            {
+                name: "google-site-verification",
+                content: "qG3soux9jAKB4Q_DYf7yj1p5cEIuib6yG4zDhpmv2_E",
+            },
+        ],
+    ],
+
+    locales: {
+        "/": {
+            lang: "en-US",
+            title: "vuepress-theme-hope",
+            description: "A vuepress theme with tons of features✨",
+        },
+        "/zh/": {
+            lang: "zh-CN",
+            title: "vuepress-theme-hope",
+            description: "一个具有强大功能的 vuepress 主题✨",
+        },
+        "/ru/": {
+            lang: "ru-RU",
+            title: "vuepress-theme-hope",
+            description: "Тема vuepress с множеством функций✨",
+        },
+    },
+
+    theme,
+
+    pagePatterns: [
+        "**/*.md",
+        "!**/*.snippet.md",
+        "!.vuepress",
+        "!node_modules",
+    ],
+
+    alias: {
+        "@IconDisplay": path.resolve(__dirname, "./components/IconDisplay"),
+        "@KatexPlayground": path.resolve(
+            __dirname,
+            "./components/KatexPlayground"
+        ),
+        "@NetlifyBadge": path.resolve(__dirname, "./components/NetlifyBadge"),
+    },
+
+    define: () => ({
+        IS_NETLIFY: "NETLIFY" in process.env,
+    }),
+});
